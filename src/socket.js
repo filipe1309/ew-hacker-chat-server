33ff7e1356c61cdb4b8c601a47e8f9698acf93f3
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-23 20:49:09 -0300

    feat: add Web Socket upgrade http connection

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,5 @@
 import http from 'http';
+import { v4 as uuid } from 'uuid';
 
 export default class SocketServer {
     constructor({ port }) {
@@ -10,6 +11,18 @@ export default class SocketServer {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
             res.end('Hey there!');
         });
+
+        server.on('upgrade', (req, socket) => {
+            socket.id = uuid();
+            const headers = [
+                'HTTP/1.1 101 Web Socket Protocol Handshake',
+                'Upgrade: Websocket',
+                'Connection: Upgrade',
+                ''
+            ].map(line => line.concat('\r\n')).join('');
+
+            socket.write(headers);
+        })
 
         return new Promise((resolve, reject) => {
             server.on('error', reject);
